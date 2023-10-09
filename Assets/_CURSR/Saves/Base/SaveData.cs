f81ff6d7f914c48c109de3cd4a77e7c38749e216
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2023-10-09 02:21:11 -0400

    added Saves category with SaveData base class

---
+++ Assets/_CURSR/Saves/Base/SaveData.cs
@@ -0,0 +1,22 @@
+﻿using CURSR.Utils;
+
+namespace CURSR.Saves
+{
+    [System.Serializable]
+    public abstract class SaveData
+    {
+        protected abstract string FileName { get; }
+        
+        private IOFile file => new IOFile(FileName);
+        
+        public virtual void Save()
+        {
+            file.Save(this);
+        }
+
+        public SaveData Load()
+        {
+            return file.Load<SaveData>();
+        }
+    }
+}
