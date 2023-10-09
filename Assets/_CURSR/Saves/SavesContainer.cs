b7747fa9f0336b1dd88ee9a01f0e6e4b75a8eba2
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2023-10-09 02:21:21 -0400

    added PlayerSave stuff

---
+++ Assets/_CURSR/Saves/SavesContainer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using CURSR.Utils;
+
+namespace CURSR.Saves
+{
+    [CreateAssetMenu(fileName = "SavesContainer", menuName = "CURSR/Container/Saves")]
+    public class SavesContainer : ScriptableObject
+    {
+        public PlayerSaveData PlayerData;
+    }
+}
