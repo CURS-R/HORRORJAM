fcf53db3a8cb365ceffc72b3cff40610220ea446
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2023-10-14 22:40:34 -0400

    Create GameObjectUtil.cs
    contains ChangeLayerRecursively methods because Unity's layer changing methodology is cringe

---
+++ Assets/_CURSR/Utils/GameObjectUtil.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace CURSR.Utils
+{
+    public static class GameObjectUtil
+    {
+        public static void ChangeLayerRecursively(Transform root, int layer)
+        {
+            root.gameObject.layer = layer;
+            foreach (Transform child in root) ChangeLayerRecursively(child, layer);
+        }
+        public static void ChangeLayerRecursively(Transform root, LayerMask layerMask)
+        {
+            int layerIndex = Mathf.RoundToInt(Mathf.Log(layerMask.value, 2)); // Convert LayerMask to layer index
+            ChangeLayerRecursively(root, layerIndex);
+        }
+    }
+}
