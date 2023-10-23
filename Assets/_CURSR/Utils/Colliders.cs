a4c60b510945cc65f125bf0daec56fe5e10859b3
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2023-10-23 01:46:13 -0400

    new Colliders util

---
+++ Assets/_CURSR/Utils/Colliders.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace CURSR.Utils
+{
+    public static class Colliders
+    {
+        public static T GetComponentUpwards<T>(GameObject obj) where T : Component
+        {
+            Transform t = obj.transform;
+
+            while (t != null)
+            {
+                T component = t.GetComponent<T>();
+                if (component != null)
+                {
+                    return component;
+                }
+                t = t.parent;
+            }
+
+            return null;
+        }
+    }
+}
